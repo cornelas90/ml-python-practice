bea6c881fd2e0f2dd3136aea3aef323821b7bed1
Christian Ornelas <81385494+cornelas90@users.noreply.github.com>
2021-05-27 00:49:39 -0500

    Update deep-learning.py

--- deep-learning.py
+++ deep-learning.py
@@ -1,5 +1,5 @@
 from sklearn.neural_network import MLPClassifier
-from sklearn.datasets import make_moons
+from sklearn.datasets import make_moons, load_breast_cancer
 from sklearn.model_selection import train_test_split
 import scipy
 import numpy as np
@@ -39,4 +39,55 @@ mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3)
 mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
 plt.xlabel('Feature 0')
 plt.ylabel('Feature 1')
+plt.show
+
+##  Checking various alpha / hidden units
+fig, axes = plt.subplots(2, 4, figsize=(20, 8))
+for axx, n_hidden_nodes in zip(axes, [10, 100]):
+	for ax, alpha in zip(axx, [0.0001, 0.01, 0.1, 1]):
+		mlp = MLPClassifier(solver='lbfgs', random_state=0, hidden_layer_sizes=[n_hidden_nodes, n_hidden_nodes], alpha=alpha)
+		mlp.fit(X_train, y_train)
+		mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3, ax=ax)
+		mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train, ax=ax)
+		ax.set_title('n_hidden=[{}, {}]\nalpha={:.4f}'.format(n_hidden_nodes, n_hidden_nodes, alpha))
+
+##  showcasing differences in models with same parameters but different initialization, based on random weight assignments
+fig, axes = plt.subplots(2, 4, figsize=(20, 8))
+for i, ax in enumerate(axes.ravel()):
+	mlp = MLPClassifier(solver='lbfgs', random_state=i, hidden_layer_sizes=[100, 100])
+	mlp.fit(X_train, y_train)
+	mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3, ax=ax)
+	mglearn.discrete_scatter(X_train[:, 0], X_train[:,1], y_train, ax=ax)
+
+##  Looking at real world example, Breast Cancer
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train,y_train)
+print('Accuracy on training set: {:.2f}'.format(mlp.score(X_train, y_train)))
+print('Accuracy on test set: {:.2f}'.format(mlp.score(X_test, y_test)))
+
+##  Improving accuracy
+mean_on_train = X_train.mean(axis=0)
+std_on_train = X_train.std(axis=0)
+X_train_scaled = (X_train - mean_on_train) / std_on_train
+X_test_scaled = (X_test - mean_on_train) / std_on_train
+
+mlp = MLPClassifier(random_state=0)
+mlp.fit(X_train_scaled, y_train)
+print('Accuracy on training set: {:.3f}'.format(mlp.score(X_train_scaled, y_train)))
+print('Accuracy on test set: {:.3f}'.format(mlp.score(X_test_scaled, y_test)))
+
+mlp = MLPClassifier(max_iter=1000, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+print('Accuracy on training set: {:.3f}'.format(mlp.score(X_train_scaled, y_train)))
+print('Accuracy on test set: {:.3f}'.format(mlp.score(X_test_scaled, y_test)))
+
+## Heatmap of weights
+plt.figure(figsize=(20, 5))
+plt.imshow(mlp.coefs_[0], interpolation='none', cmap='viridis')
+plt.yticks(range(30), cancer.feature_names)
+plt.xlabel('Columns in weight matrix')
+plt.ylabel('Input Feature')
+plt.colorbar()
 plt.show
